401d0c2245257a59deda334633a7d7cf26755601
Fernando dos Santos <nando4803@gmail.com>
2020-08-23 22:11:14 +0100

    This commit add layout

---
+++ src/Layout/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+import LeftColumn from '../LeftColumn';
+import MiddleColumn from '../MiddleColumn';
+import RightColumn from '../RightColumn';
+import MobileHeader from '../MobileHeader';
+import DeskTopHeader from '../DeskTopHeader';
+import AdBanner from '../AdBanner';
+
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      //setIsLoading(false);
+    }, 1000);
+  }, []);
+  return (
+    <Container>
+      <MobileHeader />
+      <DeskTopHeader />
+
+      <span>{!isLoading && <AdBanner />}</span>
+      <main>
+        <LeftColumn isLoading={isLoading} />
+        <MiddleColumn isLoading={isLoading} />
+        <RightColumn isLoading={isLoading} />
+      </main>
+    </Container>
+  );
+};
+
+export default Layout;
